9cb3f6b1de4242acf8b58ef5d97759e298f15323
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 17:23:09 +0200

    Implement Default Shape

--- IKS Engine/Shapes/RectangleShape.hpp
+++ IKS Engine/Shapes/RectangleShape.hpp
@@ -1,33 +1,9 @@
 #pragma once
 
+#include "DefaultShape.hpp"
+
 #include "../Packages/Vector2f.hpp"
 #include "../Packages/Color.hpp"
 
-struct RectangleShape
-{
-    Vector2f Position{};
-    Color FillColor;
-    Vector2f Size{};
-    Vector2f Origin{};
-    float Rotation;
-
-    bool IsCrashed = false;
-
-    template<typename T>
-    void FindIntersection(T *Object);
-
-    RectangleShape();
-};
-
-template<typename T>
-void RectangleShape::FindIntersection(T *Object)
-{
-    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
-        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
-
-        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
-        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
-    {
-        this->IsCrashed = true;
-    }
-}
+struct RectangleShape : public DefaultShape
+{};
