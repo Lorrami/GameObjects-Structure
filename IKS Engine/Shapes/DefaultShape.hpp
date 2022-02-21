1bb39fff695322fe12ebe56cec609077028f2feb
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 17:15:04 +0200

    Adding Default Shape

---
+++ IKS Engine/Shapes/DefaultShape.hpp
@@ -0,0 +1,14 @@
+//
+// Created by wwwro on 2/21/2022.
+//
+
+#ifndef GAMEOBJECTS_STRUCTURE_DEFAULTSHAPE_H
+#define GAMEOBJECTS_STRUCTURE_DEFAULTSHAPE_H
+
+
+class DefaultShape {
+
+};
+
+
+#endif //GAMEOBJECTS_STRUCTURE_DEFAULTSHAPE_H
