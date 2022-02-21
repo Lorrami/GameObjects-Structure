565078c7a00e5bd330f88c70aefac1bc02d13601
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 18:46:17 +0200

    Add constructor to Circle

--- IKS/Shapes/CircleShape.hpp
+++ IKS/Shapes/CircleShape.hpp
@@ -7,4 +7,12 @@
 
 struct CircleShape : public DefaultShape
 {
+	CircleShape(Vector2f Position = { 30.0f }, Color FillColor = Color::White, Vector2f Size = { 30.0f }, Vector2f Origin = { 0.0f }, float Rotation = 0)
+	{
+		this->Position = Position;
+		this->FillColor = FillColor;
+		this->Size = Size;
+		this->Origin = Origin;
+		this->Rotation = Rotation;
+	}
 };
