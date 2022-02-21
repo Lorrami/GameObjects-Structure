003117716b3e1c0338c93382ea8c6dd8ba4bdd20
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 17:12:31 +0200

    RenderWindow update

--- IKS Engine/Window/RenderWindow.hpp
+++ IKS Engine/Window/RenderWindow.hpp
@@ -1,4 +1,13 @@
-class RenderWindow
-{
+#pragma once
 
+#include "../Packages/Vector2f.hpp"
+#include "../Packages/Color.hpp"
+
+struct RenderWindow
+{
+    Vector2f VideoMode{};
+    Vector2f Position{};
+    Color FillBackground;
+
+    RenderWindow();
 };
