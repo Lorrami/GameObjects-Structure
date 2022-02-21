efa75fb08d579d3bd3cf1bddb57289c8b3bd925a
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 14:12:35 +0200

    Intersection fix

--- Shapes/RectangleShape.cpp
+++ Shapes/RectangleShape.cpp
@@ -1,6 +1,28 @@
 #include "RectangleShape.hpp"
 
-void RectangleShape::FindIntersection(GameObject *Object)
+void RectangleShape::FindIntersection(RectangleShape *Object)
+{
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
+void RectangleShape::FindIntersection(CircleShape *Object)
+{
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
+void RectangleShape::FindIntersection(LineShape *Object)
 {
     if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
         && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
--- Shapes/RectangleShape.hpp
+++ Shapes/RectangleShape.hpp
@@ -2,6 +2,8 @@
 
 #include "../Packages/Vector2f.hpp"
 #include "../Packages/Color.hpp"
+#include "CircleShape.hpp"
+#include "LineShape.hpp"
 
 struct RectangleShape
 {
@@ -12,5 +14,8 @@ struct RectangleShape
     float Rotation = 0;
 
     bool IsCrashed = false;
-    void FindIntersection(GameObject *Object);
+
+    void FindIntersection(RectangleShape *Object);
+    void FindIntersection(CircleShape *Object);
+    void FindIntersection(LineShape *Object);
 };
