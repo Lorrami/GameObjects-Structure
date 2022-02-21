db2c476ae275651a352782580023590ca574715a
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 16:16:46 +0200

    Add default constructor to Line

--- Shapes/LineShape.cpp
+++ Shapes/LineShape.cpp
@@ -1,14 +1,9 @@
 #include "LineShape.hpp"
 
-template<typename T>
-void LineShape::FindIntersection(T *Object)
+LineShape::LineShape()
 {
-    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
-        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
-
-        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
-        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
-    {
-        this->IsCrashed = true;
-    }
+    Position = {30.0f};
+    Size = {30.0f};
+    Rotation = 0;
+    FillColor = White;
 }
--- Shapes/LineShape.hpp
+++ Shapes/LineShape.hpp
@@ -9,10 +9,24 @@ struct LineShape
     Color FillColor;
     Vector2f Size;
     Vector2f Origin;
-    float Rotation = 0;
+    float Rotation;
 
     bool IsCrashed = false;
 
     template<typename T>
     void FindIntersection(T *Object);
+
+    LineShape();
 };
+template<typename T>
+void LineShape::FindIntersection(T *Object)
+{
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
