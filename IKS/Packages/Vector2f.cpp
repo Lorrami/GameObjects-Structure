cabfcd064f81272924deaa4f27818b30cae4504d
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 18:17:31 +0200

    Add constructors to Vector2f

--- IKS/Packages/Vector2f.cpp
+++ IKS/Packages/Vector2f.cpp
@@ -1,7 +1,1 @@
 #include "Vector2f.hpp"
-
-Vector2f::Vector2f(float x, float y)
-{
-    this->x = x;
-    this->y = y;
-}
--- IKS/Packages/Vector2f.hpp
+++ IKS/Packages/Vector2f.hpp
@@ -5,6 +5,17 @@ struct Vector2f
     float x;
     float y;
 public:
-    Vector2f(float x, float y);
+    Vector2f() = default;
+
+    Vector2f(float x) : Vector2f()
+    {
+        this->x = x;
+        this->y = x;
+    }
+    Vector2f(float x, float y) : Vector2f()
+    {
+        this->x = x;
+        this->y = y;
+    }
 };
 
