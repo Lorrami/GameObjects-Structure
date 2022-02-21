23a7a05609fe28c88dcdb67525a856edc4f378ad
Romcheck <wwwromchelo040608@gmail.com>
2022-02-21 13:32:26 +0200

    Adding CircleShape

---
+++ Shapes/CircleShape.cpp
@@ -0,0 +1,13 @@
+#include "CircleShape.hpp"
+
+void CircleShape::FindIntersection(GameObject *Object)
+{
+    if (this->Position.x + this->Origin.x > Object->Position.x - Object->Origin.x
+        && this->Position.y + this->Origin.y > Object->Position.y - Object->Origin.y
+
+        && this->Position.x - this->Origin.x < Object->Position.x + Object->Origin.x
+        && this->Position.y - this->Origin.y < Object->Position.y + Object->Origin.y)
+    {
+        this->IsCrashed = true;
+    }
+}
---
+++ Shapes/CircleShape.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Packages/Vector2f.hpp"
+#include "../Packages/Color.hpp"
+
+struct CircleShape
+{
+    Vector2f Position;
+    Color FillColor;
+    Vector2f Size;
+    Vector2f Origin;
+    float Rotation = 0;
+
+    bool IsCrashed = false;
+    void FindIntersection(GameObject *Object);
+};
