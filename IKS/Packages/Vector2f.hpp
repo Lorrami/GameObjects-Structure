8f448a887f5b696b832ff370bd8611236d0fdb83
Romcheck <wwwromchelo040608@gmail.com>
2022-02-23 23:46:43 +0200

    Vector2f update

--- IKS/Packages/Vector2f.hpp
+++ IKS/Packages/Vector2f.hpp
@@ -2,20 +2,11 @@
 
 struct Vector2f
 {
-    float x = 0.0f;
-    float y = 0.0f;
+    float x;
+    float y;
 public:
-    Vector2f() = default;
-
-    Vector2f(float xy) : Vector2f()
-    {
-        this->x = xy;
-        this->y = xy;
-    }
-    Vector2f(float x, float y) : Vector2f()
-    {
-        this->x = x;
-        this->y = y;
-    }
+    Vector2f();
+    Vector2f(float xy);
+    Vector2f(float x, float y);
 };
 
