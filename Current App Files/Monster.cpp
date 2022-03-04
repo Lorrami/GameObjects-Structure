fd3429f30f0f403ea387c951d6763bd463342e83
Romcheck <wwwromchelo040608@gmail.com>
2022-03-04 15:40:25 +0200

    Add Monster

---
+++ Current App Files/Monster.cpp
@@ -0,0 +1,9 @@
+#include "Monster.hpp"
+
+Monster::Monster()
+{
+    Position = 100.0f;
+    Size = 1.0f;
+    FillColor = Color::Yellow;
+    Rotation = 30;
+}
---
+++ Current App Files/Monster.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "GameObject.hpp"
+
+class Monster : public GameObject
+{
+public:
+    Monster();
+};
+
